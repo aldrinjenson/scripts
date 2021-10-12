2a5fb2a1959d9e4ca36c4d57849ea8abc7635e3b
Aldrin Jenson <aldrinjenson@gmail.com>
2021-10-12 19:59:45 +0530

    Added clipHistory file

---
+++ clipHistory.sh
@@ -0,0 +1,11 @@
+# !/bin/sh
+# Script to open clipman history menu
+# Created on 12/10/21
+
+for (( i=0 ; i<2; i++ ));
+do
+  xfce4-popup-clipman
+done
+
+
+
--- emojipicker.sh
+++ emojipicker.sh
@@ -1,12 +1,12 @@
 # !/bin/sh
 # created on 14/8/21
-# select an emoji using ctrl+shift+p binding and insert it into the current windo using xdotool
+# script to select an emoji using ctrl+shift+p binding and insert it into the current windo using xdotool
 # also have the current emoji copied to clipboard
 
 emojiFile='/home/aldrin/.scripts/assets/emojis.txt'
 selected=$(cat $emojiFile | dmenu -i -l 20)
 
-echo $selected -n | cut -d' ' -f1 | tr -d '\n' | xclip -selection clipboard
+echo $selected -n | cut -d ' ' -f1 | tr -d '\n' | xclip -selection clipboard
 xdotool key "ctrl+v"
 
 notify-send "$selected" "$selected emoji copied to clipboard" -t 1500 --icon=dialog-information
