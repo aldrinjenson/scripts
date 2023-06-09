e75e1d0db45f977452bb5b7c9c4232f58a07b184
Aldrin Jenson <aldrinjenson@gmail.com>
2023-06-09 18:52:16 +0530

    Updates

--- compresspdf.sh
+++ compresspdf.sh
@@ -3,7 +3,7 @@
 # Created on 06/05/2023
 
 filename="$@"
-ls -ll $filename
+ls -ll "$filename"
 echo "Input file = $filename"
 unalias gs && echo "Unaliased gs" || "no alias set for gs"
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$filename.compressed.pdf" "$filename" && echo "File Compressed successfully" || echo "Error in compressing"
