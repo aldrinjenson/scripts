a32db9728b0d6f4a5738a5847ebeed2570cbd857
Aldrin Jenson <aldrinjenson@gmail.com>
2023-06-11 16:45:40 +0530

    Updates

--- batteryalert.sh
+++ batteryalert.sh
@@ -13,7 +13,7 @@ echo $battery_level
 echo $battery_level $charging
 
 if [[ "$battery_level" -gt 85 && "$charging" == "true" ]] ; then
-  notify-send -u info "Battery at 85%. may want to unplug charger?"
+  notify-send -u normal "Battery at 85%." "Would be a good idea to unplug charger?"
   exit 0
 fi
 
--- syllabus.sh
+++ syllabus.sh
@@ -3,5 +3,5 @@
 # Created on 3/6/23
 
 # zathura "~/clg-stuff/Syllabus-S7&S8.pdf"
-sxiv "clg-stuff/Data Mining/dm_syllabus.png"
+zathura "clg-stuff/Data Mining/dm_syllabus.pdf"
 
