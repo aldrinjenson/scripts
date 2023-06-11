b60106a5720f5b162deb3c1784394062e5fa4222
Aldrin Jenson <aldrinjenson@gmail.com>
2023-06-11 13:49:11 +0530

    Update battery alert for cases where batteries may charge in diff order

--- batteryalert.sh
+++ batteryalert.sh
@@ -4,22 +4,24 @@
 
 # Get the current battery level
 b0_level=$(cat /sys/class/power_supply/BAT0/capacity)
-b1_level=$(cat /sys/class/power_supply/BAT1/capacity || echo 0)
+b1_level=$(cat /sys/class/power_supply/BAT1/capacity || echo 0) # to account for cases where there is only one battery
 
 battery_level=$(( ( b0_level + b1_level ) / 2 ))
-charging=$(cat /sys/class/power_supply/BAT0/status || cat /sys/class/power_supply/BAT1/status)
+charging=$(cat /sys/class/power_supply/BAT0/status /sys/class/power_supply/BAT1/status | grep -q "Charging" && echo true || echo false)
 echo $battery_level
 
-if [[ $charging = "Charging" ]]; then
+echo $battery_level $charging
+
+if [[ "$battery_level" -gt 85 && "$charging" == "true" ]] ; then
+  notify-send -u info "Battery at 85%. may want to unplug charger?"
+  exit 0
+fi
+
+if [[ $charging = "true" ]]; then
   echo "charging fine. exiting.."
   exit 0
 fi
 
-
-if [[ "$battery_level" -eq 85 && "$charging" == "Charging" ]] ; then
-  notify-send -u critical "Battery at 85%. may want to unplug charger?"
-  exit 0
-fi
 
 if [[ "$battery_level" -lt 3 ]]; then
   notify-send -u critical "Low battery! Shutting down in 10 seconds"
