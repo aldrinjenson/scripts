7fd2b887748b4768c10f4f579d0c71f80858f1ce
Aldrin Jenson <aldrinjenson@gmail.com>
2023-06-19 07:13:37 +0530

    Updates

--- syllabus.sh
+++ syllabus.sh
@@ -2,6 +2,5 @@
 # simple script to show CS syllabus 
 # Created on 3/6/23
 
-# zathura "~/clg-stuff/Syllabus-S7&S8.pdf"
-zathura ~/clg-stuff/Blockchain/blockchain-syllabus.pdf
+zathura "~/clg-stuff/Syllabus-S7&S8.pdf"
 
