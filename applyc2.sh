4908fe72782b2719c777bc9eaee205f0d5a3a2e4
Aldrin Jenson <aldrinjenson@gmail.com>
2023-09-20 22:10:45 +0530

    Updates

--- applyc2.sh
+++ applyc2.sh
@@ -1,5 +1,5 @@
-# !/bin/sh
+#!/bin/sh
 # Script to connect to ApplyWiz Server
 # Created on 19/9/23
-#
-ssh -i "~/personal/applywizPem.pem" admin@ec2-3-26-240-150.ap-southeast-2.compute.amazonaws.com
+
+ssh -i "~/personal/applyWizServer.pem" admin@ec2-13-126-206-188.ap-south-1.compute.amazonaws.com
