3dc93b273fe4b5e8d116014a737099130e66d44f
Aldrin Jenson <aldrinjenson@gmail.com>
2023-12-27 00:34:53 +0530

    Updates

--- musiclyrics/getJsonlyrics.sh
+++ musiclyrics/getJsonlyrics.sh
@@ -2,8 +2,7 @@
 # script to get the timestamped json lyrics of a song using openai whisper
 # created on 26/5/23
 
-unalias whisper
-if [ !$1 ];
+if [ ! "$1" ];
 then
   echo "Song path not passed. exiting"
   exit 1
@@ -11,6 +10,10 @@ fi
 
 echo $1
 
-whisper --model base.en "$1" --output_dir ~/Music/json/ --output_format json --task transcribe
+# first convert to wav file using ffmpeg and store in /tmp
+# then apply whisper and save the data to Music/json/filename
+# then delete the wav file
+
+# whisper --m ~/whisper-experiments/whisper.cpp/models/ggml-base.en.bin --output-json "$1" --output_dir ~/Music/json/ --output_format json --task transcribe
 
 
