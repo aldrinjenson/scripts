eaeefaac054543291d70c614c34714f857263e51
Aldrin Jenson <aldrinjenson@gmail.com>
2024-01-28 14:34:40 +0530

    Updates

--- fixtouch.sh
+++ fixtouch.sh
@@ -2,11 +2,12 @@
 # Script to disable keyboard for 1 minute or for the passed duration
 # Created on 20/11/21
 
-clientId=15
+# clientId=11
 masterId=2
 # get the Ids using 'xinput list' command
 
 clientId=$(xinput list | grep "Multi"| cut -d = -f2 | cut -f1)
+echo $clientId
 duration=${1-1}
 
 notify-send "Disabling Touchscreen $clientId" "Duration: $duration minute"
