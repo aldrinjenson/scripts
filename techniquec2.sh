38dc9a0f39d805bf40a40569f30bf504812f1e14
Aldrin Jenson <aldrinjenson@gmail.com>
2024-02-17 04:19:48 +0530

    Updates

---
+++ techniquec2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to connect to Technique ec2 server 
+# Created on 17/2/24
+
+techniqueSshCmd=$(getEnv.sh  techniqueSshCmd)
+echo $techniqueSshCmd
+
+# ssh -X -o ServerAliveInterval=600 "$username"@"$url" 
+# ssh -X "$(techniqueSshCmd)"
+$techniqueSshCmd
